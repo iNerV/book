1b8929e73091396ff60948e02d4e303ce7850459
NerV <voldemar.krs@gmail.com>
2014-10-27 16:59:37 +0700

    rating

--- books/models.py
+++ books/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from users.models import MyUser
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
+    author_id = models.CharField(max_length=200)
     gender = models.CharField(max_length=6, null=True, blank=True)
-    birth_date = models.CharField(max_length=200, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     site = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Series(models.Model):
-    gr_id = models.PositiveIntegerField()
     name = models.CharField(max_length=200)
+    gr_id = models.PositiveIntegerField()
     desc = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class GrId(models.Model):
@@ -24,7 +32,7 @@ class Book(models.Model):
     ru_desc = models.TextField(null=True)
     en_desc = models.TextField(null=True)
     num_series = models.PositiveIntegerField(blank=True, null=True)
-    gr_id = models.PositiveIntegerField()
+    gr_id = models.PositiveIntegerField(editable=False, blank=True, null=True)
     series = models.ManyToManyField(Series, blank=True, null=True)
     author = models.ManyToManyField(Author)
 
@@ -56,10 +64,20 @@ class ASIN(models.Model):
 
 
 class Covers(models.Model):
-    cover = models.ImageField(upload_to='/covers/', max_length=200)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    cover = models.ImageField(upload_to='covers/', max_length=200)
     book = models.ForeignKey(Book, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Photos(models.Model):
-    photo = models.ImageField(upload_to='/authors_photo/', max_length=200, null=True)
+    photo = models.ImageField(upload_to='authors_photo/', max_length=200, null=True)
     author = models.ForeignKey(Author, blank=True, null=True)
+
+
+class Rating(models.Model):
+    book = models.ForeignKey(Book)
+    user = models.ForeignKey(MyUser)
+    rating = models.IntegerField()
